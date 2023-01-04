e80a5710d01676a3e2cbedfee4926478c0d11590
SandeshBashyal <sandeshbashyal01@gmail.com>
2023-01-04 09:12:06 +0545

    testing static

--- djangoreactjs/settings.py
+++ djangoreactjs/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-b9@n5nviz*%%gzv8_yyrtq_d60ed_ubk$7w%-qlxt5!n$r&#x8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['project.lichal.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -108,7 +108,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS =[
-    os.path.join(BASE_DIR, 'blogreact/build/static')
+    os.path.join(os.path.join(BASE_DIR, 'blogreact'),'build','static')
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
